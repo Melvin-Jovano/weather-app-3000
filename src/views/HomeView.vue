78ef9ee920e2e5f84585b5b0a864c43b53bcd861
Melvin-Jovano <melvinjovano2@gmail.com>
2023-05-03 11:35:08 +0700

    feat: navbar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe necessitatibus recusandae, asperiores ratione vitae expedita. Ipsum, dolorem, dolore labore inventore a suscipit nesciunt assumenda molestias hic accusantium, consequuntur porro magni?
 </template>
+
+<script setup>
+  import TheWelcome from '../components/TheWelcome.vue'
+</script>
