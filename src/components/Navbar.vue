48df92b9ad439e505b5efbf15c410ffee7b74ceb
Melvin-Jovano <melvinjovano2@gmail.com>
2023-05-05 14:19:53 +0700

    feat: navbar links

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -29,7 +29,10 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <router-link class="nav-link" active-class="active" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" active-class="active" to="/favourites">Favourites</router-link>
           </li>
         </ul>
         <form class="d-flex">
---
+++ src/views/FavouritesView.vue
@@ -0,0 +1,7 @@
+<template>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae similique debitis voluptates ex quam molestiae excepturi quas amet ad nostrum dicta in, impedit temporibus obcaecati vel nemo iste eaque eligendi.
+</template>
+
+<script setup>
+
+</script>
